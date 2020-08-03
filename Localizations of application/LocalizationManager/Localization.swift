75b3c6467cf58d5c3de2790128e28c5d6bcdd107
VitaliiDeveloper <lavreniuk.vitalii@gmail.com>
2020-08-03 18:31:34 +0300

    Updated LocalizationManager and Example

--- Localizations of application/LocalizationManager/Localization.swift
+++ Localizations of application/LocalizationManager/Localization.swift
@@ -11,38 +11,64 @@ import Cocoa
 import UIKit
 #endif
 
-//At this enum add all of AppLanguages
+enum AppLanguagesCode:String {
+  case ru_UA
+  case en_UK
+}
+
 enum AppLanguages:String {
+  case ru
   case en
 }
 
-//At this enum add .strings files to search localizations
 enum AppLanguageFiles:String {
-  case Localization
+  case Localizable
 }
 
 class Localization {
-  private static var bundle:Bundle?
+  static let main = Localization()
   
-  static func get(key:LocalizationKeys, file:AppLanguageFiles = .Localization) -> String {
-    if Localization.getUsingLanguage() != nil {
+  private var bundle:Bundle?
+  
+  private var _usageAppLanguageCode:AppLanguagesCode = .ru_UA
+  var usageAppLanguageCode:AppLanguagesCode {
+    return _usageAppLanguageCode
+  }
+  
+  private var _usageAppLanguage:AppLanguages = .ru
+  var usageAppLanguage:AppLanguages {
+    return _usageAppLanguage
+  }
+  
+  func get(key:LocalizationKeys, file:AppLanguageFiles = .Localizable) -> String {
+    if self.getUsingLanguage() != nil {
       return bundle!.localizedString(forKey: key.rawValue, value: nil, table: file.rawValue)
     } else {
       return "Need to set Localization language!"
     }
   }
   
-  static func set(language:AppLanguages) {
+  func set(language:AppLanguages) {
     guard let path = Bundle.main.path(forResource: language.rawValue, ofType: "lproj") else {
       assertionFailure("path doesn't available!")
       return
     }
     
+    self._usageAppLanguage = language
+    
+    switch language {
+    case .ru:
+      self._usageAppLanguageCode = .ru_UA
+    case .en:
+      self._usageAppLanguageCode = .en_UK
+    }
+    
     self.bundle = Bundle(path: path)
     UserDefaults.standard.set(language.rawValue, forKey: "application-language")
+    UserDefaults.standard.set([self._usageAppLanguageCode.rawValue], forKey: "AppleLanguages")
   }
   
-  static func getUsingLanguage() -> String? {
+  func getUsingLanguage() -> String? {
     if self.bundle != nil {
       return UserDefaults.standard.string(forKey: "application-language")
     } else {
